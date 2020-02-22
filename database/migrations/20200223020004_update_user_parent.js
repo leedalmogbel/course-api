13e23ad0e711583e48e8165df6a5e41aa99b88d2
Ciara Jhenica Estavillo <estavillocj43@gmail.com>
2020-02-23 02:40:00 +0800

    Update 20200223020004_update_user_parent.js

--- database/migrations/20200223020004_update_user_parent.js
+++ database/migrations/20200223020004_update_user_parent.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
       return knex.schema
         .renameTable('user_parent', 'user_parents')
         .alterTable('user_parents', (table) => {
+          table.renameColumn('user_active', 'user_parent_active')
           table.dateTime('user_parent_created').notNull();
           table.dateTime('user_parent_updated').notNull();
         });
